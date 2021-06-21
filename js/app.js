e4c1709e8c891a7a5b3e3e3fea62945c9d5e5ef8
Aleksey <50425794+AlekseyKuk@users.noreply.github.com>
2021-06-22 02:13:06 +0500

    Plugin email
    Use plugin for sending messages to mail

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+
+function OpenMenu() {
+    document.getElementById("menu-mobil").style.transform = "translateX(0%)";
+}
+function closeMenu() {
+    document.getElementById("menu-mobil").style.transform = "translateX(-100%)";
+}
+
+		
+}
